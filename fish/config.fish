6c802a85e994c0bb89bf0d0c425f8c63820bd5e2
Oscar Söderlund <odsod@users.noreply.github.com>
2025-03-18 10:17:50 +0100

    feat(fish): set nord theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,44 @@
 set fish_greeting
+
 if status is-interactive
     starship init fish | source
 end
+
+# Colorscheme: Nord
+set -U fish_color_normal normal
+set -U fish_color_command 88c0d0
+set -U fish_color_quote a3be8c
+set -U fish_color_redirection b48ead --bold
+set -U fish_color_end 81a1c1
+set -U fish_color_error bf616a
+set -U fish_color_param d8dee9
+set -U fish_color_comment 4c566a --italics
+set -U fish_color_match --background=brblue
+set -U fish_color_selection d8dee9 --bold --background=434c5e
+set -U fish_color_search_match --bold --background=434c5e
+set -U fish_color_history_current e5e9f0 --bold
+set -U fish_color_operator 81a1c1
+set -U fish_color_escape ebcb8b
+set -U fish_color_cwd 5e81ac
+set -U fish_color_cwd_root bf616a
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 4c566a
+set -U fish_color_user a3be8c
+set -U fish_color_host a3be8c
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress 3b4252 --background=d08770
+set -U fish_pager_color_completion e5e9f0
+set -U fish_pager_color_description ebcb8b --italics
+set -U fish_pager_color_selected_background --background=434c5e
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_secondary_prefix
+set -U fish_pager_color_selected_completion
+set -U fish_color_option
+set -U fish_pager_color_selected_description
+set -U fish_pager_color_selected_prefix
+set -U fish_pager_color_secondary_description
+set -U fish_color_keyword
+set -U fish_pager_color_background
+set -U fish_color_host_remote
+set -U fish_pager_color_secondary_completion
