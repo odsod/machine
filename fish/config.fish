8ccf63d70e48d576584379a12c8650a72f9b0ccd
Oscar Söderlund <odsod@users.noreply.github.com>
2026-02-07 22:07:06 +0100

    fix(fish): minor tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -1,54 +1,52 @@
-if test "$PAGER" = "head -n 10000 | cat" -o "$COMPOSER_NO_INTERACTION" = "1"
-    return
-end
-
-alias vim="nvim"
-alias vi="nvim"
-
 set fish_greeting
-
-if status is-interactive
-    starship init fish | source
-end
 
 # Hostname-specific history
 set -x fish_history (string replace -a '-' '_' (hostname))
 
+if not status is-interactive
+    return
+end
+
+abbr -a vim nvim
+abbr -a vi nvim
+
+starship init fish | source
+
 # Colorscheme: Nord
-set -U fish_color_normal normal
-set -U fish_color_command 88c0d0
-set -U fish_color_quote a3be8c
-set -U fish_color_redirection b48ead --bold
-set -U fish_color_end 81a1c1
-set -U fish_color_error bf616a
-set -U fish_color_param d8dee9
-set -U fish_color_comment 4c566a --italics
-set -U fish_color_match --background=brblue
-set -U fish_color_selection d8dee9 --bold --background=434c5e
-set -U fish_color_search_match --bold --background=434c5e
-set -U fish_color_history_current e5e9f0 --bold
-set -U fish_color_operator 81a1c1
-set -U fish_color_escape ebcb8b
-set -U fish_color_cwd 5e81ac
-set -U fish_color_cwd_root bf616a
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 4c566a
-set -U fish_color_user a3be8c
-set -U fish_color_host a3be8c
-set -U fish_color_cancel --reverse
-set -U fish_pager_color_prefix normal --bold --underline
-set -U fish_pager_color_progress 3b4252 --background=d08770
-set -U fish_pager_color_completion e5e9f0
-set -U fish_pager_color_description ebcb8b --italics
-set -U fish_pager_color_selected_background --background=434c5e
-set -U fish_pager_color_secondary_background
-set -U fish_pager_color_secondary_prefix
-set -U fish_pager_color_selected_completion
-set -U fish_color_option
-set -U fish_pager_color_selected_description
-set -U fish_pager_color_selected_prefix
-set -U fish_pager_color_secondary_description
-set -U fish_color_keyword
-set -U fish_pager_color_background
-set -U fish_color_host_remote
-set -U fish_pager_color_secondary_completion
+set -g fish_color_normal normal
+set -g fish_color_command 88c0d0
+set -g fish_color_quote a3be8c
+set -g fish_color_redirection b48ead --bold
+set -g fish_color_end 81a1c1
+set -g fish_color_error bf616a
+set -g fish_color_param d8dee9
+set -g fish_color_comment 4c566a --italics
+set -g fish_color_match --background=brblue
+set -g fish_color_selection d8dee9 --bold --background=434c5e
+set -g fish_color_search_match --bold --background=434c5e
+set -g fish_color_history_current e5e9f0 --bold
+set -g fish_color_operator 81a1c1
+set -g fish_color_escape ebcb8b
+set -g fish_color_cwd 5e81ac
+set -g fish_color_cwd_root bf616a
+set -g fish_color_valid_path --underline
+set -g fish_color_autosuggestion 4c566a
+set -g fish_color_user a3be8c
+set -g fish_color_host a3be8c
+set -g fish_color_cancel --reverse
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress 3b4252 --background=d08770
+set -g fish_pager_color_completion e5e9f0
+set -g fish_pager_color_description ebcb8b --italics
+set -g fish_pager_color_selected_background --background=434c5e
+set -g fish_pager_color_secondary_background
+set -g fish_pager_color_secondary_prefix
+set -g fish_pager_color_selected_completion
+set -g fish_color_option
+set -g fish_pager_color_selected_description
+set -g fish_pager_color_selected_prefix
+set -g fish_pager_color_secondary_description
+set -g fish_color_keyword
+set -g fish_pager_color_background
+set -g fish_color_host_remote
+set -g fish_pager_color_secondary_completion
