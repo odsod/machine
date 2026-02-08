963dec79909471fc11a7bbde07ef3dcd5168ea27
Oscar Söderlund <odsod@users.noreply.github.com>
2026-02-08 11:58:54 +0100

    feat(fish): update config

--- fish/config.fish
+++ fish/config.fish
@@ -9,44 +9,96 @@ end
 
 abbr -a vim nvim
 abbr -a vi nvim
+abbr -a cat bat
+abbr -a find fd
+
+# Enable Vi mode
+fish_vi_key_bindings
 
 starship init fish | source
+fzf --fish | source
+
+# Bindings
+function fish_user_key_bindings
+    fzf_key_bindings
+
+    # --- Global Bindings (Insert & Normal) ---
+    
+    # Ctrl-f: Find File (Smart Base) - CLEAN
+    set -l find_file_clean '
+        set -l root (git rev-parse --show-toplevel 2>/dev/null)
+        if test -z "$root"
+            string match -q "$HOME*" $PWD; and set root $HOME; or set root "/"
+        end
+        set -x FZF_DEFAULT_COMMAND "fd --type f --base-directory $root --strip-cwd-prefix --exclude .git"
+        fzf-file-widget
+    '
+    bind \cf $find_file_clean
+    bind -M insert \cf $find_file_clean
+
+    # Alt-f: Find File (Smart Base) - ALL (Hidden + No Ignore)
+    set -l find_file_all '
+        set -l root (git rev-parse --show-toplevel 2>/dev/null)
+        if test -z "$root"
+            string match -q "$HOME*" $PWD; and set root $HOME; or set root "/"
+        end
+        set -x FZF_DEFAULT_COMMAND "fd --type f --base-directory $root --strip-cwd-prefix --hidden --no-ignore --exclude .git"
+        fzf-file-widget
+    '
+    bind \ef $find_file_all
+    bind -M insert \ef $find_file_all
+
+    # --- Normal Mode Bindings ---
+
+    # u: Up Directory
+    bind -M default u 'cd ..; commandline -f repaint'
+
+    # p: Project Directory Jump (Clean)
+    # Logic: Nearest Git Root -> Current Dir
+    bind -M default p '
+        set -l root (git rev-parse --show-toplevel 2>/dev/null)
+        if test -z "$root"
+            set root "."
+        end
+        set -l target (fd --type d --base-directory "$root" --strip-cwd-prefix --exclude .git | fzf --prompt="Project CD> ")
+        if test -n "$target"
+            cd "$root/$target"
+            commandline -f repaint
+        end
+    '
+
+    # P: Project Directory Jump (All - Hidden + Ignored)
+    # Logic: Nearest Git Root -> Current Dir
+    bind -M default P '
+        set -l root (git rev-parse --show-toplevel 2>/dev/null)
+        if test -z "$root"
+            set root "."
+        end
+        set -l target (fd --type d --base-directory "$root" --strip-cwd-prefix --hidden --no-ignore --exclude .git | fzf --prompt="Project ALL> ")
+        if test -n "$target"
+            cd "$root/$target"
+            commandline -f repaint
+        end
+    '
+
+    # c: Code Directory Jump (Clean)
+    bind -M default c '
+        set -l target (fd --type d --base-directory $HOME/Code --strip-cwd-prefix --exclude .git | fzf --prompt="Code CD> ")
+        if test -n "$target"
+            cd "$HOME/Code/$target"
+            commandline -f repaint
+        end
+    '
+
+    # C: Code Directory Jump (All - Hidden + Ignored)
+    bind -M default C '
+        set -l target (fd --type d --base-directory $HOME/Code --strip-cwd-prefix --hidden --no-ignore --exclude .git | fzf --prompt="Code ALL> ")
+        if test -n "$target"
+            cd "$HOME/Code/$target"
+            commandline -f repaint
+        end
+    '
+end
 
 # Colorscheme: Nord
-set -g fish_color_normal normal
-set -g fish_color_command 88c0d0
-set -g fish_color_quote a3be8c
-set -g fish_color_redirection b48ead --bold
-set -g fish_color_end 81a1c1
-set -g fish_color_error bf616a
-set -g fish_color_param d8dee9
-set -g fish_color_comment 4c566a --italics
-set -g fish_color_match --background=brblue
-set -g fish_color_selection d8dee9 --bold --background=434c5e
-set -g fish_color_search_match --bold --background=434c5e
-set -g fish_color_history_current e5e9f0 --bold
-set -g fish_color_operator 81a1c1
-set -g fish_color_escape ebcb8b
-set -g fish_color_cwd 5e81ac
-set -g fish_color_cwd_root bf616a
-set -g fish_color_valid_path --underline
-set -g fish_color_autosuggestion 4c566a
-set -g fish_color_user a3be8c
-set -g fish_color_host a3be8c
-set -g fish_color_cancel --reverse
-set -g fish_pager_color_prefix normal --bold --underline
-set -g fish_pager_color_progress 3b4252 --background=d08770
-set -g fish_pager_color_completion e5e9f0
-set -g fish_pager_color_description ebcb8b --italics
-set -g fish_pager_color_selected_background --background=434c5e
-set -g fish_pager_color_secondary_background
-set -g fish_pager_color_secondary_prefix
-set -g fish_pager_color_selected_completion
-set -g fish_color_option
-set -g fish_pager_color_selected_description
-set -g fish_pager_color_selected_prefix
-set -g fish_pager_color_secondary_description
-set -g fish_color_keyword
-set -g fish_pager_color_background
-set -g fish_color_host_remote
-set -g fish_pager_color_secondary_completion
+fish_config theme choose Nord
