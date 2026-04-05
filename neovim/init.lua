ba4f45d0aad738d69bec43af4e4c3bc4ec70de40
Oscar Söderlund <odsod@users.noreply.github.com>
2026-04-05 15:36:38 +0200

    feat(neovim): migrate to vim.pack
    - replace lazy.nvim with native vim.pack (Neovim 0.12.0)
    - drop nvim-lspconfig (config already uses native LSP APIs)
    - batch all plugins in single vim.pack.add() for parallel install
    - add vim.loader.enable() for startup performance
    - switch Makefile from tarball-only to tarball + vim.pack.update()

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,14 +1,10 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
+vim.loader.enable()
 
 -- Leader key
 vim.g.mapleader = " "
 
 require("odsod.plugins").setup()
+require("odsod.lsp").setup()
 require("odsod.ui.statusline").setup()
 require("odsod.core.diagnostics").setup()
 require("odsod.core.options").setup()
--- neovim/lua/odsod/plugins.lua
+++ neovim/lua/odsod/plugins.lua
@@ -1,70 +1,52 @@
 local M = {}
 
 function M.setup()
-  require("lazy").setup({
-    -- Theme: Nord
-    {
-      "shaunsingh/nord.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.g.nord_italic = false
-        vim.g.nord_bold = false
-        require("nord").set()
-        vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })
-      end,
-    },
+  -- PackChanged hook must be registered BEFORE vim.pack.add()
+  vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(ev)
+      if ev.data.spec.name == "nvim-treesitter" then
+        vim.cmd("TSUpdate")
+      end
+    end,
+  })
 
-    -- UI & Tools
-    {
-      "nvim-tree/nvim-tree.lua",
-      opts = {
-        filters = {
-          git_ignored = false,
-        },
-        filesystem_watchers = {
-          ignore_dirs = {
-            "node_modules",
-            ".git",
-            ".jj",
-          },
-        },
-        view = { width = 30 },
-        renderer = {
-          group_empty = true,
-          icons = { show = { file = false, folder = false, folder_arrow = true, git = false } },
-        },
-      },
-    },
-    {
-      "ibhagwan/fzf-lua",
-      config = function()
-        require("odsod.ui.fzf").setup()
-      end,
-    },
-    { "numToStr/Comment.nvim", opts = {} },
+  -- Install all plugins (parallel download on first run)
+  vim.pack.add({
+    "https://github.com/shaunsingh/nord.nvim",
+    "https://github.com/nvim-tree/nvim-tree.lua",
+    "https://github.com/ibhagwan/fzf-lua",
+    "https://github.com/numToStr/Comment.nvim",
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+  })
 
-    -- Treesitter: Better syntax highlighting
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      opts = {
-        ensure_installed = { "go", "python", "lua", "bash", "json", "yaml", "markdown", "proto" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      },
-      config = function(_, opts)
-        require("nvim-treesitter").setup(opts)
-      end,
-    },
+  -- Theme: Nord
+  vim.g.nord_italic = false
+  vim.g.nord_bold = false
+  require("nord").set()
+  vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })
 
-    -- LSP: Language Support
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-        require("odsod.lsp").setup()
-      end,
+  -- File tree
+  require("nvim-tree").setup({
+    filters = { git_ignored = false },
+    filesystem_watchers = { ignore_dirs = { "node_modules", ".git", ".jj" } },
+    view = { width = 30 },
+    renderer = {
+      group_empty = true,
+      icons = { show = { file = false, folder = false, folder_arrow = true, git = false } },
     },
+  })
+
+  -- Fuzzy finder
+  require("odsod.ui.fzf").setup()
+
+  -- Comments
+  require("Comment").setup({})
+
+  -- Treesitter
+  require("nvim-treesitter").setup({
+    ensure_installed = { "go", "python", "lua", "bash", "json", "yaml", "markdown", "proto" },
+    highlight = { enable = true },
+    indent = { enable = true },
   })
 end
 
